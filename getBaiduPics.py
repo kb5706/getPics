f68c8330b0a59ba559be9cbf8bca8f1a860f044d
Neal <kb5706@gmail.com>
2018-12-06 17:54:31 +0800

    1st change
    1st change

---
+++ getBaiduPics.py
@@ -0,0 +1,9 @@
+import requests
+import re
+
+#用输入的关键词拼接出搜索后的页面url
+print('请输入搜索关键词')
+keyword = str(input())
+url = 'http://image.baidu.com/search/flip?tn=baiduimage&ie=utf-8&word=' + keyword + '&ct=201326592&v=flip'
+result = requests.get(url).text  #得到搜索页的源码
+model =  re.compile('"objURL":"(.*?)",', re.S) #用正则表达式定义原图地址的规律
