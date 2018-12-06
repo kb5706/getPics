49b06e1a943cc9c5246900cc52ba4e36dee1feba
Neal <kb5706@gmail.com>
2018-12-06 23:46:07 +0800

    add

--- getBaiduPics.py
+++ getBaiduPics.py
@@ -5,5 +5,7 @@ import re
 print('请输入搜索关键词')
 keyword = str(input())
 url = 'http://image.baidu.com/search/flip?tn=baiduimage&ie=utf-8&word=' + keyword + '&ct=201326592&v=flip'
-result = requests.get(url).text  #得到搜索页的源码
+pagecode = requests.get(url).text  #得到搜索页的源码
 model =  re.compile('"objURL":"(.*?)",', re.S) #用正则表达式定义原图地址的规律
+pic_url = re.findall('"objURL":"(.*?)",',pagecode,re.S)#匹配网页源代码中所有原图的地址
+
